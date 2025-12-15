987eaade6d4128d08051d5c4a323468aa894a699
Rujuta-133 <rantrolikar@gmail.com>
2025-12-15 22:28:15 +0530

    chore: add sample movie and rating data + run_chunk1 script (chunk1)

---
+++ data/sample_data.py
@@ -0,0 +1,28 @@
+movies = [
+    {"movie_id": 1, "title": "Inception", "genres": ["Sci-Fi", "Action"], "rating_sum": 13, "rating_count": 2, "year": 2010, "imdb_rating": 8.8},
+    {"movie_id": 2, "title": "The Notebook", "genres": ["Romance"], "rating_sum": 15, "rating_count": 3, "year": 2004, "imdb_rating": 7.8},
+    {"movie_id": 3, "title": "The Grudge", "genres": ["Horror"], "rating_sum": 9, "rating_count": 4, "year": 2004, "imdb_rating": 5.9},
+    {"movie_id": 4, "title": "Spirited Away", "genres": ["Anime"], "rating_sum": 15, "rating_count": 2, "year": 2001, "imdb_rating": 8.6},
+    {"movie_id": 5, "title": "Mission Impossible", "genres": ["Action"], "rating_sum": 11, "rating_count": 1, "year": 1996, "imdb_rating": 7.2},
+    {"movie_id": 6, "title": "The Proposal", "genres": ["Romance", "Comedy"], "rating_sum": 8, "rating_count": 2, "year": 2009, "imdb_rating": 6.8},
+    {"movie_id": 7, "title": "Interstellar", "genres": ["Sci-Fi"], "rating_sum": 20, "rating_count": 4, "year": 2014, "imdb_rating": 8.7},
+    {"movie_id": 8, "title": "Batman Begins", "genres": ["Action", "Crime"], "rating_sum": 10, "rating_count": 2, "year": 2005, "imdb_rating": 8.2},
+    {"movie_id": 9, "title": "The Holiday", "genres": ["Romance", "Comedy"], "rating_sum": 8, "rating_count": 3, "year": 2006, "imdb_rating": 7.0},
+    {"movie_id": 10, "title": "Jerry Maguire", "genres": ["Comedy"], "rating_sum": 14, "rating_count": 3, "year": 1996, "imdb_rating": 7.9}
+
+]
+
+
+ratings = [
+    {"user_id": "user_1", "movie_id": 2, "rating_value": 4.5},
+    {"user_id": "user_2", "movie_id": 1, "rating_value": 5.0},
+    {"user_id": "user_3", "movie_id": 4, "rating_value": 3.5},
+    {"user_id": "user_4", "movie_id": 7, "rating_value": 4.5},
+    {"user_id": "user_5", "movie_id": 3, "rating_value": 4.5},
+    {"user_id": "user_2", "movie_id": 5, "rating_value": 4.6},
+    {"user_id": "user_2", "movie_id": 8, "rating_value": 4.7},
+    {"user_id": "user_1", "movie_id": 6, "rating_value": 4.3},
+    {"user_id": "user_5", "movie_id": 2, "rating_value": 4.5},
+    {"user_id": "user_4", "movie_id": 1, "rating_value": 4.5}, 
+    {"user_id": "user_1", "movie_id": 9, "rating_value": 4.8}  
+]
---
+++ run_chunk1.py
@@ -0,0 +1,5 @@
+from data.sample_data import movies, ratings
+
+print("Number of movies: ", len(movies))
+print("Number of ratings: ", len(ratings))
+print("Sample movie[0] genres: ",movies[0]["genres"])
